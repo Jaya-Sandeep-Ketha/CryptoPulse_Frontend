153e8229cda3852b21c056d3df89c083bfd263b1
Jaya-Sandeep-Ketha <kethajayasandeep1254@gmail.com>
2025-01-03 23:39:33 -0500

    Update api.js

--- src/Api/api.js
+++ src/Api/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 const DEPLOYED='https://e-commerce-server-production-0873.up.railway.app'
 // const LOCALHOST='http://localhost:5454'
-const LOCALHOST='https://cryptopulse-app.onrender.com:5454'
+const LOCALHOST='https://cryptopulse-app.onrender.com'
 
 export const API_BASE_URL = LOCALHOST
 
